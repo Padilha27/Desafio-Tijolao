773acd083e4adacae94fb9703c7f7c5eee3c6446
Bianca Padilha <83797611+Padilha27@users.noreply.github.com>
2022-01-31 10:56:27 -0300

    Add files via upload
    Parte front

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="cellphone">
+  <div class="screen"></div>
+
+  <div class="buttonsLine">
+    <div class="button">1<span>.!@</span></div>
+    <div class="button">2<span>abc</span></div>
+    <div class="button">3<span>def</span></div>
+  </div>
+  <div class="buttonsLine">
+    <div class="button">4<span>ghi</span></div>
+    <div class="button">5<span>jkl</span></div>
+    <div class="button">6<span>mno</span></div>
+  </div>
+  <div class="buttonsLine">
+    <div class="button">7<span>pqrs</span></div>
+    <div class="button">8<span>tuv</span></div>
+    <div class="button">9<span>wxyz</span></div>
+  </div>
+  <div class="buttonsLine">
+    <div class="button">*<span>[]</span></div>
+    <div class="button">0<span>+</span></div>
+    <div class="button">#<span>&</span></div>
+  </div>
+
+
+</div>
+`;
